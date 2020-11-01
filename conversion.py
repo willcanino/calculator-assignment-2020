c34921032ca7bc5db0583430eeeab9d4cfeb8b20
Will Canino <willcaninoyt@gmail.com>
2020-11-02 07:02:49 +1000

    finished conversion.py adding new features in time.py

--- conversion.py
+++ conversion.py
@@ -10,23 +10,26 @@ print("1. Fahrenheit to Celsius"); time.sleep(1);
 print("2. Celsius to Fahrenheit"); time.sleep(1);
 print("3. Miles to Kilometers"); time.sleep(1);
 print("4. Kilometers to Miles"); time.sleep(1);
+print();
 
-num1 = int(input("Which conversion would u like: ")); time.sleep(1);
+num1 = int(input("Which conversion would you like to use: ")); time.sleep(1);
 
 if num1 == 1:
-    cel = int(input("Enter your number in Degrees Celsius: ")); time.sleep(1);
-    fah = (cel*9/5)+32;
-    print("The temperature in Fahrenheit is: ", fah);
-
-if num1 == 2:
     fah = int(input("Enter your number in Fahrenheit: ")); time.sleep(1);
     cel = (fah - 32)*5/9;
-    print("The temperature in Degrees Celsius is: ", cel);
+    print("The temperature in Degrees Celsius is: ", cel); time.sleep(1);
+
+if num1 == 2:
+    cel = int(input("Enter your number in Degrees Celsius: ")); time.sleep(1);
+    fah = (cel*9/5)+32;
+    print("The temperature in Fahrenheit is: ", fah); time.sleep(1);
 
 if num1 == 3:
-    km = int(input("Enter your number in Kilometers: ")); time.sleep(1);
-    m = (km*1.609);
-    print(km, "Kilometers is", m, "Miles (approximately)");
+    m = int(input("Enter your number in Miles: ")); time.sleep(1);
+    km = (m*1.609);
+    print(m, "Miles is", km, "Kilometers (approximately)"); time.sleep(1);
 
 if num1 == 4:
-    int(input("Enter your number in Kilometers: ")); time.sleep(1);
+    km = int(input("Enter your number in Kilometers: ")); time.sleep(1);
+    m = (km/1.609);
+    print(km, "Kilometers is", m, "Miles (approximately)"); time.sleep(1);
--- menu.py
+++ menu.py
@@ -1,13 +1,16 @@
 import time
 
-print("Welcome to this calculator"); time.sleep(1);
+print("****************************************");
+print("*****Welcome to the Calculator Menu*****");
+print("****************************************");
 print();
+
 print("This calculator will contain:"); time.sleep(2);
 print("1. Normal Calculator"); time.sleep(1);
 print("2. Conversion Calculator"); time.sleep(1);
 print();
 
-num1 = int(input("Select which calculator you want: "));
+num1 = int(input("Select which calculator you would like to use: ")); time.sleep(1);
 
 if num1 == 1:
     import calculator

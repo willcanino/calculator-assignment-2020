497ba3f657d557b11a86681cc0fc4becc4a312eb
Will Canino <willcaninoyt@gmail.com>
2020-11-01 21:59:29 +1000

    finished 3 equations

--- calculator.py
+++ calculator.py
@@ -3,18 +3,15 @@ print("*****Welcome to my Calculator*****");
 print("**********************************");
 print();
 
-# Take initial numbers from the user
 num1 = input("Enter your first number: ");
 num2 = input("Enter your second number: ");
 
-# Convert the text (str) we got from the user to an int
 num1 = int(num1);
 num2 = int(num2);
 
 while True:
   operator = input("What operator do you want to use? [ + | - | * | / ]");
 
-  # These if statements determine what operator the user inputted
   if operator == '+':
     result = num1 + num2
   elif operator == '-':
@@ -25,25 +22,16 @@ while True:
     result = num1 / num2
   else:
     print(f"{operator} is an invalid operator");
-    # This skips the rest of lines of code in the loop and starts at the
-    # beginning
     continue
 
-  # print out the result
   print(f"{num1} {operator} {num2} = {result}");
 
-  # We need to set num1 = result so that when the loop repeats the if statement
-  # the result we be passed along instead of just using the old num1 value
-  # in the computation
   num1 = result
 
   response = input("Do you want to run another calculation? [Y/N]");
   if response == "n" or response == "N" or response == "no" or response == "No":
-    # This stops the loops
     print()
     print("Thank you for using this calculator! :)");
     break
-  # This must be set to num2 in order for the if statements above to work
   num2 = input("Enter your next number: ");
-  # Convert num2 from a str to an int
   num2 = int(num2);
--- conversion.py
+++ conversion.py
@@ -1,13 +1,32 @@
-print("Welcome to the Conversion Calculator");
+import time
 
+print("**********************************************");
+print("*****Welcome to the Conversion Calculator*****");
+print("**********************************************");
+print();
 
-print("Which conversion would you like to do?");
-print("1. Fahrenheit to Celsius");
-print("2. Celcius to Fahrenheit");
-print("3. Miles to Kilometers");
-print("4. Kilometers to Miles");
+print("Which conversion would you like to do?"); time.sleep(1);
+print("1. Fahrenheit to Celsius"); time.sleep(1);
+print("2. Celsius to Fahrenheit"); time.sleep(1);
+print("3. Miles to Kilometers"); time.sleep(1);
+print("4. Kilometers to Miles"); time.sleep(1);
 
-num1 = int(input("Which conversion would u like: "));
+num1 = int(input("Which conversion would u like: ")); time.sleep(1);
 
 if num1 == 1:
-    print int(input("Enter your first number: "));
+    cel = int(input("Enter your number in Degrees Celsius: ")); time.sleep(1);
+    fah = (cel*9/5)+32;
+    print("The temperature in Fahrenheit is: ", fah);
+
+if num1 == 2:
+    fah = int(input("Enter your number in Fahrenheit: ")); time.sleep(1);
+    cel = (fah - 32)*5/9;
+    print("The temperature in Degrees Celsius is: ", cel);
+
+if num1 == 3:
+    km = int(input("Enter your number in Kilometers: ")); time.sleep(1);
+    m = (km*1.609);
+    print(km, "Kilometers is", m, "Miles (approximately)");
+
+if num1 == 4:
+    int(input("Enter your number in Kilometers: ")); time.sleep(1);

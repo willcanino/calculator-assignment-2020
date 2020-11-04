2b9f1b4ef196bbb640af9ecd2c0924b4329b49e0
Will Canino <willcaninoyt@gmail.com>
2020-11-04 21:10:36 +1000

    edited time.py

--- time.py
+++ time.py
@@ -27,10 +27,12 @@ print("18. Minutes to Weeks");
 print("19. Weeks to Minutes");
 print("20. Minutes to Months");
 print("21. Months to Minutes");
-print("22. Minutes to Months");
-print("23. Minutes to Months");
-print("24. Minutes to Months");
-print("25. Minutes to Months");
+print("22. Minutes to Years");
+print("23. Years to Minutes");
+print("24. Minutes to Decades");
+print("25. Decades to Minutes");
+print("20. Minutes to Centurys");
+print("20. Centurys to Minutes");
 print("20. Minutes to Months");
 print("20. Minutes to Months");
 print("20. Minutes to Months");
@@ -44,5 +46,20 @@ print("20. Minutes to Months");
 print("20. Minutes to Months");
 print("20. Minutes to Months");
 print("20. Minutes to Months");
-print("20. Minutes to Months");
-print("20. Minutes to Months");
+
+num1 = int(input("Enter which time conversion you would like to use: "));
+
+if num1 == 1:
+    sec = int(input("Enter the amount of seconds you would like to convert to minutes: "));
+    min = (sec/60);
+    print(sec,"seconds is", min,"minutes.");
+
+if num1 == 2:
+    min = int(input("Enter the amount of minutes you would like to convert to seconds: "));
+    sec = (min*60);
+    print(min,"minutes is",sec,"seconds.")
+
+if num1 == 3:
+    sec = int(input("Enter the amount of seconds you would like to convert to hours: "));
+    hrs = (sec*3600);
+    print(sec,"seconds is",hrs,"hours")
